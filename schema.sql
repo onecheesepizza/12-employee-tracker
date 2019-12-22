e7587e64b3c56a633b9f5b0cf6663a93e5c26c18
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-12-21 22:13:38 -0800

    add auto_increment to db schema

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE database employeesDB;
 USE employeesDB;
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
